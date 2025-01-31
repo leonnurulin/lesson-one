66660a6e30d6330087c2341748978767a928d0a0
leonnurulin <nurulinleonid310@gmail.com>
2025-01-31 16:51:37 +0300

    сделал первый урок

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+using UnityEngine; //это библиотека
+
+public class HelloWorld : MonoBehaviour
+{
+    // привет!
+    void Start()
+    {
+        print("Hello World!!!!");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
