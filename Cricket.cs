000b81697141066c0245244e362305b9856a6535
leonnurulin <nurulinleonid310@gmail.com>
2025-02-05 17:13:25 +0300

    Add files via upload

---
+++ Cricket.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Cricket : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Cricket
+    void Update()
+    {
+        print("Crickets");
+    }
+}
---
+++ DestroyObject.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Destroy(gameObject); // Уничтожение объекта
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
